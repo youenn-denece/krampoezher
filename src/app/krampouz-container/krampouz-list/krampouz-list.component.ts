b46d12c714ab077155b9bf7dcac9f70c7bbc2d03
youenn-denece <youenn.denece@gmail.com>
2023-10-09 22:23:34 +0200

    clean

--- src/app/krampouz-container/krampouz-list/krampouz-list.component.ts
+++ src/app/krampouz-container/krampouz-list/krampouz-list.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Krampouz } from 'src/app/shared/interfaces/krampouz.interface';
 
-
-
 @Component({
   selector: 'app-krampouz-list',
   templateUrl: './krampouz-list.component.html',
