200a504b003ff5fbbc8c9f8d03b980afc158c5ee
youenn-denece <youenn.denece@gmail.com>
2023-10-09 22:26:19 +0200

    delete tests classes

--- src/app/shared/directives/selected.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SelectedDirective } from './selected.directive';
-
-describe('SelectedDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SelectedDirective();
-    expect(directive).toBeTruthy();
-  });
-});
